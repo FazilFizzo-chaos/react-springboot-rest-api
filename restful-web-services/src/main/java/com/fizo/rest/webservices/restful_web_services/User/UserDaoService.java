9253e324bd5548a2314b3aa0c6e5aff8d46db7ea
FazilFizzo-chaos <156065220+FazilFizzo-chaos@users.noreply.github.com>
2024-08-06 10:30:27 +0300

    implemented exception handling on all resources
    created package "exception" on path "com/fizo/rest/webservices/restful_web_services/exception" and implemented customized ResponseEntity Exception and also created ErrorDetails class that will appear on Response headers when error occurs. Also created UserNotFoundException class on User package that will handle exception when user not found, found on path "com/fizo/rest/webservices/restful_web_services/User/UserNotFoundException.java"

--- restful-web-services/src/main/java/com/fizo/rest/webservices/restful_web_services/User/UserDaoService.java
+++ restful-web-services/src/main/java/com/fizo/rest/webservices/restful_web_services/User/UserDaoService.java
@@ -5,18 +5,37 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
 
     public static List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
 
     static {
-        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Dan", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Dan", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
     }
+
     public List<User> findAll() {
         return users;
+    }
+
+    //using predicate and lambda expression to get user by id
+    public User findUserById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream()
+                .filter(predicate)
+                .findFirst()
+                .orElse(null);
+    }
+
+    //creating and saving new User
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
     }
 }
